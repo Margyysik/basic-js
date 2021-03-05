5630cf6295c6e2a4e93728e8eff9c9a644a51bd6
Margyysik <71218459+Margyysik@users.noreply.github.com>
2021-03-05 19:21:32 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LN2 = 0.693;
 
 module.exports = function dateSample(sampleActivity) {
   if (
